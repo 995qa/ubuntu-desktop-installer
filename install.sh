9e86b579cc560ff41b3ed9ec99a2028d2b45891c
Steven Cole <67975160+ZackeryCole@users.noreply.github.com>
2022-01-21 15:15:11 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,8 @@ echo 'Ubuntu Desktop Installer'
 echo 'ZCY'
 echo 'NOTICE: Use your Internet.'
 sudo apt install ubuntu-Desktop
+sudo apt install lightdm
+sudo rc-service enable lightdm
 echo 'Done! If you see errors, Try Again or Report an Issue.'
+echo 'ALSO! do "sudo rc-service start lightdm" to start the Display Env.'
+echo 'But it will automaticly do that on boot.'
