75ad776b1fa14b3a8771b2777bbba7d8d65a9977
995 <67975160+995qa@users.noreply.github.com>
2024-01-04 01:47:41 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ echo 'NOTICE: Use your Internet.'
 sudo apt install ubuntu-Desktop
 sudo apt install lightdm
 sudo rc-service enable lightdm
-echo 'Done! If you see errors, Try Again or Report an Issue.'
-echo 'ALSO! do "sudo rc-service start lightdm" to start the Display Env.'
-echo 'But it will automaticly do that on boot.'
+echo 'Done! If you see errors, Try again or Report an Issue.'
+echo 'ALSO! use "sudo rc-service start lightdm" to start the Display Env.'
+echo 'But it will automatically do that on boot.'
